cd91b60e3cfcb2bde9082dc69ff9e8b44da9ed27
Zachary Martin <78982358+wolfboy580@users.noreply.github.com>
2021-02-12 14:28:18 -0600

    May Festival

---
+++ May Festival/ViewController.swift
@@ -0,0 +1,23 @@
+//
+//  ViewController.swift
+//  May Festival
+//
+//  Created by Juan Herrera on 2/6/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var Button: UIButton!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    @IBAction func ButtonClick(_ sender: Any) {
+        print("the button was clicked")
+    }
+    
+}
+
