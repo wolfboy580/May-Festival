8c6295e5f615ad00e9dbf3c4bc8d32c5a32761da
Zachary Martin <78982358+wolfboy580@users.noreply.github.com>
2021-02-26 01:08:49 -0600

    Added Sound

--- May Festival/ViewController.swift
+++ May Festival/ViewController.swift
@@ -6,24 +6,112 @@
 //
 
 import UIKit
+import AVFoundation
+
 
 class ViewController: UIViewController {
+    var audioPlayer = AVAudioPlayer()
 
     @IBOutlet weak var Button: UIButton!
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+ 
         // Do any additional setup after loading the view.
     }
 
     @IBAction func ButtonClick2(_ sender: Any) {
-        print("the button was clicked")
+        print("button pressed")
 
+        if let soundURL = Bundle.main.url(forResource: "Pew", withExtension: "wav") {
+
+        do {
+
+        audioPlayer = try AVAudioPlayer(contentsOf: soundURL)
+
+        }
+
+        catch {
+
+        print(error)
+
+        }
+
+        
+
+        }else{
+
+        print("Unable to locate audio file")
+
+        }
+        
+        
+        audioPlayer.play()
+        
+        
+            
     }
     @IBAction func Button3(_ sender: Any) {
         print("the button was clicked")
+        
+        print("button pressed")
+
+        if let soundURL = Bundle.main.url(forResource: "Cymbal", withExtension: "mp3") {
+
+        do {
+
+        audioPlayer = try AVAudioPlayer(contentsOf: soundURL)
+
+        }
+
+        catch {
+
+        print(error)
+
+        }
+
+        
+
+        }else{
+
+        print("Unable to locate audio file")
+
+        }
+        
+        
+        audioPlayer.play()
+        
     }
     @IBAction func ButtonClick(_ sender: Any) {
         print("the button was clicked")
+        print("button pressed")
+
+        if let soundURL = Bundle.main.url(forResource: "High Tom", withExtension: "mp3") {
+
+        do {
+
+        audioPlayer = try AVAudioPlayer(contentsOf: soundURL)
+
+        }
+
+        catch {
+
+        print(error)
+
+        }
+
+        
+
+        }else{
+
+        print("Unable to locate audio file")
+
+        }
+        
+        
+        audioPlayer.play()
+        
+        
             
     }
     
